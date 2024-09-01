e5066bc67560359592c31474051c020e5fcc11d5
War-assassin123 <zachriperger@gmail.com>
2024-09-01 10:45:50 -0500

    Main menu added
    added the buttons and object to manage data persistence throughout scenes

---
+++ Assets/Scripts/DataManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+    public static DataManager instance;
+    public string playerName;
+}
